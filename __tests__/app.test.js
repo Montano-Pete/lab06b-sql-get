db35c92c79c6aaecc19cbb00ef45f6536b0dac07
Peter <pete225848@gmail.com>
2021-06-16 16:18:57 -0700

    successfully TDD both endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,31 +31,92 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns fruits', async() => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+            id: 1,
+            name: 'Rainier Cherry',
+            category: 'Tree',
+            price: 1,
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
+            id: 2,
+            name: 'Mango',
+            category: 'Tree',
+            price: 3,
         },
         {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
+            id: 3,
+            name: 'Strawberry',
+            category: 'Ground',
+            price: 2,
+        },
+        {
+            id: 4,
+            name: 'Grapefruit',
+            category: 'Tree',
+            price: 5,
+        },
+        {
+            id: 5,
+            name: 'Watermelon',
+            category: 'Ground',
+            price: 7,
+        },
+        {
+            id: 6,
+            name: 'Kiwi',
+            category: 'Vine',
+            price: 2,
+        },
+        {
+            id: 7,
+            name: 'Passion Fruit',
+            category: 'Vine',
+            price: 4,
+        },
+        {
+            id: 8,
+            name: 'Lychee',
+            category: 'Tree',
+            price: 1,
+        },
+        {
+            id: 9,
+            name: 'Lemon',
+            category: 'Tree',
+            price: 2,
+        },
+        {
+            id: 10,
+            name: 'Dragon Fruit',
+            category: 'Cactus',
+            price: 4,
+        },
+      ];
+
+      const data = await fakeRequest(app)
+        .get('/fruits')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('returns fruit specific id', async() => {
+
+      const expectation = [
+        {
+            id: 1,
+            name: 'Rainier Cherry',
+            category: 'Tree',
+            price: 1,
         }
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/fruits/1')
         .expect('Content-Type', /json/)
         .expect(200);
 
